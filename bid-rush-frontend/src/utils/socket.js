ffee08c790c51a2746cea0b1116c06702777888f
Satyam-here15 <satyam.work15@gmail.com>
2026-06-03 11:52:05 +0530

    feat: update frontend URLs to production

--- bid-rush-frontend/src/utils/socket.js
+++ bid-rush-frontend/src/utils/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5004", {
+const socket = io("https://bidrush-notification.onrender.com", {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
 });
 
-export default socket; 
+export default socket;
