adba13157a12df2707fff94396ee841439f3e1b0
Satyam-here15 <satyam.work15@gmail.com>
2026-06-05 12:55:54 +0530

    fix: dotenv override false for render compatibility

--- gateway/index.js
+++ gateway/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config({ override: false })
 
 const app = express()
 
